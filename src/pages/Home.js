afed86db2de812dcdf132eaade0f6d9235f8a23f
Igor Seliversotv <igrseliwjorstow@bk.ru>
2020-12-06 21:17:03 +0400

    finish

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Search} from "../components/Search";
 import {Card} from "../components/Card";
+import {GithubContext} from "../context/github/githubContext";
 
 export const Home = () => {
+
+	const {loading, users} = useContext(GithubContext)
+
 	return (
 		<>
 			<Search />
 
-			<div className="row">
-				{[1, 2, 3].map((item, index) => {
-					return (
-						<div key={index} className="col-sm-4 mb-4">
-							<Card />
-						</div>
-					)
-				})}
-			</div>
+			{
+				loading
+				? <p className="text-center">Загрузка...</p>
+				: <div className="row">
+						{users.map(user => (
+							<div key={user.id} className="col-sm-4 mb-4">
+								<Card user={user} />
+							</div>
+						))}
+					</div>
+			}
+
 		</>
 	)
 }
